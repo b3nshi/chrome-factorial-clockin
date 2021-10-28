e25f379b674614aeb13216e34db2b08e1de608e5
Benjamin Simon <b3nshi@gmail.com>
2021-10-28 13:41:43 +0200

    Clean and organize tools to force guidelines

--- Readme.md
+++ Readme.md
@@ -1,19 +1,25 @@
-# Factorial Autofill
+# Factorial Clock-In Autofill
 
 It is a simple Chrome extension developed to autofill the clock-in option in Factorial.
 
-> Note: This is not an official tool. It does not require any sensitive data, and you can always check the code and import it manually in Chrome.
+> Note: This is not an official tool. It does not get access to any sensitive data, and you can always check the code and import it manually in Chrome.
 
-# How to install it?
+### Features
+
+- Definition of multiple ranges
+- Ignore weekends
+- Ignore "Holidays"
+
+### How to install it?
 
 1. Go to chrome://extensions and enable `developer mode` (the option should be located in the top-right corner)
 2. Click on `Load unpacked`
-3. Select the folder with the code
+3. Select the folder `src` with the code
 4. Enjoy!
 
 > I don't see any real benefit in building the code since it is an extension meant to be installed manually at the moment, so you can download this folder and use it as-is.
 
-# How to build the extension
+### How to build the extension
 
 1. Run `npm install`
 2. Run `npm build`
--- src/popup.js
+++ src/popup.js
@@ -1,5 +1,4 @@
-const BASE_HOST_STORAGE = "site_";
-const KNOWN_URLS = ["app.factorialhr.com"];
+const KNOWN_URLS = ['app.factorialhr.com'];
 
 const autofill = function () {
   const LOGGER = false;
@@ -8,22 +7,22 @@ const autofill = function () {
     LOGGER && console.log(...messages);
   };
 
-  function autofillExecution(arrayOfTimes) {
-    logger("Running code with:", arrayOfTimes);
+  const autofillExecution = (arrayOfTimes) => {
+    logger(`Running code with: ${arrayOfTimes}`);
 
     const pressKey = (el, key) => {
       el.dispatchEvent(
-        new KeyboardEvent("keydown", {
+        new KeyboardEvent('keydown', {
           keyCode: key.charCodeAt(0),
           bubbles: true,
         })
       );
     };
 
-    // Format of text should be: HH:mm
+    // Text format should be: HH:mm
     const writeText = (el, text) => {
-      el.value = "";
-      el.dispatchEvent(new Event("focus"));
+      el.value = '';
+      el.dispatchEvent(new Event('focus'));
       pressKey(el, text[0]);
       pressKey(el, text[1]);
       pressKey(el, text[3]);
@@ -31,36 +30,29 @@ const autofill = function () {
     };
 
     const iterateOverTimeRows = (containerTimeRows, start = 0) => {
-      const timeRows = containerTimeRows.querySelectorAll(":scope > div");
+      const timeRows = containerTimeRows.querySelectorAll(':scope > div');
       logger(`Time rows: ${timeRows.length} - Start: ${start}`);
       Array.from(timeRows)
         .slice(start)
         .forEach((timeRow, index) => {
-          // This is a corrected index, just to optimize the update
           const i = index + start;
-          // The time for this row has been defined
           logger(`Index is: ${i} and ${arrayOfTimes[i]}`);
           if (arrayOfTimes[i]) {
-            const inputs = timeRow.querySelectorAll(":scope input");
-            logger("Found inputs:", inputs.length);
+            const inputs = timeRow.querySelectorAll(':scope input');
+            logger('Found inputs:', inputs.length);
 
             if (inputs.length && !inputs[0].disabled) {
               writeText(inputs[0], arrayOfTimes[i][0]);
               writeText(inputs[1], arrayOfTimes[i][1]);
 
-              // Now the button should be visible
-              const buttons = timeRow.querySelectorAll(":scope button");
+              const buttons = timeRow.querySelectorAll(':scope button');
               logger(`Total buttons: ${buttons.length}`);
               if (
                 (i === 0 && buttons.length === 2) ||
                 (i > 0 && buttons.length === 1)
               ) {
-                // This means the times haave changed
                 buttons[0].click();
-
-                // Check if we need to add more times
                 if (i < arrayOfTimes.length - 1) {
-                  // We reached the end of the lines created here
                   if (timeRows.length === index + 1) {
                     buttons[1].click();
                     iterateOverTimeRows(timeRow.parentNode, index + 1);
@@ -73,36 +65,46 @@ const autofill = function () {
     };
 
     const iterateOverDays = (tableRows) => {
-      logger("Starting iteration to update all the times");
+      logger('Starting iteration to update all the times');
       Array.from(tableRows).forEach((tr) => {
-        if (tr.className.indexOf("disabled") === -1) {
-          const tds = tr.getElementsByTagName("td");
+        if (tr.className.indexOf('disabled') === -1) {
+          const tds = tr.getElementsByTagName('td');
           const firstTD = tds[0];
           const day = firstTD
-            .getElementsByTagName("div")[0]
-            .getElementsByTagName("div")[1].innerHTML;
+            .getElementsByTagName('div')[0]
+            .getElementsByTagName('div')[1].innerHTML;
 
-          if (!["s", "d", "S", "D"].includes(day[0])) {
-            const secondTD = tds[1].getElementsByTagName("div")[0];
+          let isVacation = false;
+          try {
+            isVacation = !!firstTD
+              .getElementsByTagName('div')[0]
+              .getElementsByTagName('div')[2];
+          } catch (e) {
+            logger(
+              'it was not found an special label to indicate it is a holiday'
+            );
+          }
+
+          if (!isVacation && !['s', 'd', 'S', 'D'].includes(day[0])) {
+            const secondTD = tds[1].getElementsByTagName('div')[0];
             iterateOverTimeRows(secondTD);
           }
         }
       });
     };
 
-    // The clock-in page has only one table
     const body = document
-      .getElementsByTagName("table")[0]
-      .getElementsByTagName("tbody")[0];
-    const trs = body.getElementsByTagName("tr");
+      .getElementsByTagName('table')[0]
+      .getElementsByTagName('tbody')[0];
+    const trs = body.getElementsByTagName('tr');
 
     iterateOverDays(trs);
-  }
+  };
 
-  chrome.storage.sync.get("arrayOfTimes", function (value) {
-    logger("The value is:", value);
-    autofillExecution(JSON.parse(value.arrayOfTimes || "[]"));
-    chrome.storage.sync.remove("arrayOfTimes");
+  chrome.storage.sync.get('arrayOfTimes', function (value) {
+    logger('The value is:', value);
+    autofillExecution(JSON.parse(value.arrayOfTimes || '[]'));
+    chrome.storage.sync.remove('arrayOfTimes');
   });
 };
 
@@ -111,9 +113,9 @@ const isKnownURL = (url) => {
 };
 
 window.onload = async function () {
-  const lightbox = document.getElementById("lightbox");
+  const lightbox = document.getElementById('lightbox');
 
-  var tabInfo = false;
+  let tabInfo = false;
 
   try {
     const [tab] = await chrome.tabs.query({
@@ -126,25 +128,25 @@ window.onload = async function () {
   }
 
   if (tabInfo === false || !isKnownURL(tabInfo.url)) {
-    lightbox.className = "visible";
+    lightbox.className = 'visible';
     return;
   } else {
-    lightbox.className = "";
+    lightbox.className = '';
   }
 
   const arrayOfTimeRanges = () => {
-    const times = document.querySelectorAll("#times > .time-range");
+    const times = document.querySelectorAll('#times > .time-range');
     const arrayOfTimes = [];
     Array.from(times).forEach((timeContainer) => {
       arrayOfTimes.push([
-        timeContainer.querySelector("input[name='text-from']").value,
-        timeContainer.querySelector("input[name='text-to']").value,
+        timeContainer.querySelector('input[name="text-from"]').value,
+        timeContainer.querySelector('input[name="text-to"]').value,
       ]);
     });
     return JSON.stringify(arrayOfTimes);
   };
 
-  document.getElementById("btn-autofill").onclick = function () {
+  document.getElementById('btn-autofill').onclick = function () {
     chrome.storage.sync.set(
       {
         arrayOfTimes: arrayOfTimeRanges(),
@@ -162,27 +164,27 @@ window.onload = async function () {
     e.target.parentNode.parentNode.remove();
   };
 
-  const timesContainer = document.getElementById("times");
-  document.getElementById("btn-newline").onclick = function () {
+  const timesContainer = document.getElementById('times');
+  document.getElementById('btn-newline').onclick = function () {
     timesContainer.innerHTML =
       timesContainer.innerHTML +
       `
-    <div class="row time-range">
-      <div class="col-sm-4">
-        <input type="time" class="tertiary" name="text-from" />
-      </div>
-      <div class="col-sm-4">
-        <input type="time" class="tertiary" name="text-to" />
-      </div>
-      <div class="col-sm-4">
-        <button name="btn-remove-line">Remove</button>
-      </div>
-    </div>
+        <div class="row time-range">
+          <div class="col-sm-4">
+            <input type="time" class="tertiary" name="text-from" />
+          </div>
+          <div class="col-sm-4">
+            <input type="time" class="tertiary" name="text-to" />
+          </div>
+          <div class="col-sm-4">
+            <button name="btn-remove-line">Remove</button>
+          </div>
+        </div>
     `;
 
     const newButton = document.querySelector(
-      "#times .time-range:last-child button"
+      '#times .time-range:last-child button'
     );
-    newButton.addEventListener("click", removeElement);
+    newButton.addEventListener('click', removeElement);
   };
 };
